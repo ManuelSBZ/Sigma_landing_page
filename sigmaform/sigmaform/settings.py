7e30997d228fabe20470c842c1c6affd0981047f
Manuel Semeco <manusbz98@gmail.com>
2020-08-27 08:53:29 -0500

    heroku config

--- sigmaform/sigmaform/settings.py
+++ sigmaform/sigmaform/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'wb-yu##&51)++=i5!uf3^@kqs0+s7ey0114)tzryt$69--+1_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare'
 ]
 
 ROOT_URLCONF = 'sigmaform.urls'
@@ -119,8 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles" )
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static" )
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- sigmaform/sigmaform/urls.py
+++ sigmaform/sigmaform/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings,Settings
+from django.conf.urls.static import static
+from .settings import STATIC_ROOT
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sigmapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root = STATIC_ROOT)
